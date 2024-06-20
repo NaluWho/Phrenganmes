d375aaba1a6b896490cc06bc8a8094447404c735
VNalu <veronica.n.pollock@gmail.com>
2024-06-19 20:01:01 -0700

    refactored tierList into new file

--- public/scenes/combinedResults.js
+++ public/scenes/combinedResults.js
@@ -9,14 +9,16 @@ export class CombinedResults extends Phaser.Scene {
         
         this.socket = this.registry.get('socket');
         this.socket.emit('combinedResults');
-        
+
         this.height = this.cameras.main.height;
         this.width = this.cameras.main.width;
         this.tierRectHeight = this.height*0.11;
         this.tierRectWidth = this.width*0.09;
 
-        var parentScene = this.scene.get('SoloTierList');
-        parentScene.createTierLayout(this);
+        // var parentScene = this.scene.get('SoloTierList');
+        // parentScene.createTierLayout(this);
+        var tierList = new TierList(this);
+        tierList.createTierLayout(this);
     }
 
 }
